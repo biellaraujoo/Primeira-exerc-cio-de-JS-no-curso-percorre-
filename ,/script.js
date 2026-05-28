3966afc57ccbbf3013612302d2dfe6edbdd5f470
biellaraujoo <gabriel.araujo100310@gmail.com>
2026-05-28 13:31:25 -0300

    atividade JS

---
+++ ,/script.js
@@ -0,0 +1,13 @@
+alert ("Boas vindas ao nosso site de hamburgeria");
+alert ("Lanches disponíveis: X-salada, X-bacon, X-tudo" );
+alert ("Bebidas disponíveis: chopp, Suco, Refrigerante" );
+let lanche = prompt ("Qual o seu pedido?");
+console.log (lanche);
+let bebida = prompt ("Qual a sua bebida?");
+console.log (bebida);
+alert ("Seus pedidos foram: " + lanche + " " + bebida);
+let compra = prompt ("qual a forma de pagamento?");
+console.log (compra);
+alert ("A transação foi aceita")
+alert ("logo mais chega em sua residência")
+alert ("Obrigado pela preferência, Volte sempre !!!")
---
+++ script.js
@@ -0,0 +1,7 @@
+let nomeUsuario = prompt ("Digite o seu nome, por favor.");
+console.log (nomeUsuario);
+let idade = prompt ("qual a sua idade?");
+console.log (idade);
+let endereco = prompt ("Qual o seu endereço?");
+console.log (endereco);
+alert ("Seu nome é " + nomeUsuario + "?  " + "Sua idade é " + idade + "?  " +"seu endereço é " + endereco + "?")
